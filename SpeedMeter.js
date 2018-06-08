077518a30303b7c15e2c9c2eb80a0ebe2160054d
Sam B <sam2b@uw.edu>
2018-06-08 14:38:48 -0700

    v2018.6.8
    _Depends on Zwifter.js
    _SpeedMeter.js many additions to accommodate modes, and automatic resistance changes.
    _Zwifter.js requires your Zwift username, password, and ID number.

--- SpeedMeter.js
+++ SpeedMeter.js
@@ -1,133 +1,215 @@
-var SpeedMeter = function(SensorPin, levelUpPin, levelDownPin, weighting, pulsesPerRev, timeOut, bounceTime) {
+var level = 1;
+var sleep = require('system-sleep');
+var gpio = require('rpi-gpio');
+var onoff = require('onoff').Gpio; //include onoff to interact with the GPIO
+var relay;
+var maxLevel;
+var autoMode = true;
+var oldPower = 0;
+var oldRPM = 0;
+
+var SpeedMeter = function(SensorPin, levelUpPin, levelDownPin, resistUpPin, resistDownPin, weighting, pulsesPerRev, timeOut, bounceTime) {
   var rpm = 0;
-  var level = 1;
   var timeAct = 0;
   var timeOld = 0;
   var timeDiff = 0;
   var timeLevel = 0;
   var watchdog = null;
-  var gpio = require('rpi-gpio');
   var power = [
-    [8,16,25,38,50,69,88,103,120,138,152],
-    [11,22,34,51,69,91,113,131,154,178,192],
-    [12,24,42,64,88,114,145,175,202,238,254],
-    [15,31,51,77,107,140,170,206,236,280,294],
-    [19,38,60,91,126,163,203,243,283,327,367],
-    [20,40,69,105,145,188,228,273,319,362,409],
-    [22,48,79,119,164,214,260,310,360,400,458],
-    [24,53,88,133,183,237,286,350,406,447,518],
-    [28,57,97,146,202,263,318,389,449,494,585],
-    [30,64,106,160,221,289,337,415,476,518,620],
-    [32,68,115,174,240,312,351,435,499,544,655],
-    [33,72,123,186,259,338,293,483,550,596,712],
-    [37,79,133,200,278,361,427,527,592,652,783],
-    [40,87,142,215,297,387,462,571,636,709,832],
-    [42,91,151,228,316,409,485,599,669,744,864],
-    [44,95,160,242,335,436,515,633,708,787,905]
-   ];
-  
-  
-  weighting = weighting || 0;
-  pulsesPerRev = pulsesPerRev || 1;
-  timeOut = timeOut || 10000;
-  bounceTime = bounceTime || 200;
-  
-  gpio.on('change', function(channel, value)
-  {
-    if (channel == SensorPin)
-    {
-      var timeAct = new Date();
-      if ((timeAct - timeOld) > bounceTime)
-      {
-        if (watchdog)
-        {
-          clearTimeout(watchdog);
+    //[6,12,20,29,40,53,69,79,92,106,121],
+    //[8,16,26,38,53,68,88,103,120,138,152],
+    [9,20,32,47,66,84,107,125,148,172,186],    // Level 1
+    [11,23,39,56,79,101,126,150,173,206,219],
+    [13,27,45,65,92,117,145,175,202,238,254],
+    [15,31,52,75,105,135,166,202,231,275,289],
+    [16,35,58,85,118,152,185,226,260,305,332],
+    [18,39,65,96,131,169,208,249,289,333,375],
+    [19,42,71,104,144,184,227,272,318,361,408],
+    [21,46,77,113,157,199,245,295,345,386,442], // Level 8 (0% grade)
+    [23,50,84,123,170,216,262,318,372,413,480],
+    [24,53,89,131,183,230,279,342,398,441,512],
+    [26,56,94,139,196,245,296,365,424,468,548],
+    [28,60,101,148,209,261,318,389,449,494,585],
+    [30,64,108,158,222,277,337,415,476,518,620],
+    [32,68,115,168,235,296,355,439,503,548,658],
+    [33,72,122,177,248,312,373,463,530,576,694],
+    [35,76,129,187,261,328,390,484,556,606,727],
+    [37,79,134,195,274,342,407,507,572,632,763],
+    [39,83,140,204,287,354,424,528,598,659,790],
+    [40,87,146,213,300,368,442,551,616,689,812],
+    [42,91,153,223,313,385,461,574,645,720,840], // Level 20
+    //[44,95,160,234,326,401,479,598,673,752,872],
+    //[47,101,171,246,340,418,501,625,706,788,908]
+    ];
+
+    maxLevel = power.length; // subtract the starting level so it is relative to zero.
+    weighting = weighting || 0;
+    pulsesPerRev = pulsesPerRev || 1; // The quantity of magnets on the wheel.
+    timeOut = timeOut || 10000
+    /* Between 200-350 will help reduce multiple level changes during a single physical button press.
+       The goal is to have as large of a value as possible and still have as quick of a change in physical level as possible.
+       So you might have to tune and tweak the bounceTime value and the relay delay value together.
+    */
+    bounceTime = bounceTime || 175;
+    
+    gpio.on('change', function(channel, value) {
+        switch(channel) {
+            case SensorPin: // Every 1 RPM.
+                var timeAct = new Date();
+                if ((timeAct - timeOld) > bounceTime) {
+                    if (watchdog) {
+                        clearTimeout(watchdog); // Cancel the function.
+                        // But, if the RPM signal stops (aka: you stop pedaling), then the watchdog function will eventually set your RPM to 0.
+                    }
+                    if (timeOld) {
+                        timeDiff *= weighting;
+                        timeDiff += (1 - weighting) * (timeAct - timeOld);
+                        rpm = 60000 / (timeDiff * pulsesPerRev);
+                    }
+                    timeOld = timeAct;
+                    watchdog = setTimeout(function() { // When you stop pedaling.
+                        console.log("------------------------------------------");
+                        console.log("-- -- -- WATCHDOG SET RPM TO ZERO -- -- --"); // debugging.
+                        console.log("------------------------------------------");
+                        timeOld = 0;
+                        rpm = 0;
+                        },  timeOut);
+                }
+                break;
+            case levelUpPin:
+                var timeLevelUp = new Date();
+                if (!autoMode && (timeLevelUp - timeLevel) > bounceTime) {
+                    if (level < maxLevel) {
+                      ++level;
+                      console.log("     MANUAL LEVEL " + level);
+                    }
+                    timeLevel = timeLevelUp;
+                }
+                break;
+            case levelDownPin:
+                var timeLevelDown = new Date();
+                if (!autoMode && (timeLevelDown - timeLevel) > bounceTime) {
+                    if (level > 1) {
+                      --level;
+                      console.log("    MANUAL  LEVEL " + level);
+                    }
+                    timeLevel = timeLevelDown;
+                }
+                break;
+            default:
+                console.log("ERROR, unknown channel number " + channel);
         }
-        if (timeOld)
-        {
-          timeDiff *= weighting;
-          timeDiff += (1 - weighting) * (timeAct - timeOld);
-          rpm = 60000 / (timeDiff * pulsesPerRev);
-        }
-        timeOld = timeAct;
-        watchdog = setTimeout(function()
-        {
-          timeOld = 0;
-          rpm = 0;
-        }, timeOut);
-      }
-    }
-    else if (channel == levelUpPin)
-    {
-      var timeLevelUp = new Date();
-      if ((timeLevelUp - timeLevel) > bounceTime)
-      {
-        if (level < 16)
-        {
-          ++level;
-        }
-        timeLevel = timeLevelUp;
-      }
-    }
-    else if (channel == levelDownPin)
-    {
-      var timeLevelDown = new Date();
-      if ((timeLevelDown - timeLevel) > bounceTime)
-      {
-        if (level > 1)
-        {
-          --level;
-        }
-        timeLevel = timeLevelDown;
-      }
-    }
-  });
+    });
+
+    gpio.setup(SensorPin, gpio.DIR_IN, gpio.EDGE_RISING);
+    gpio.setup(levelUpPin, gpio.DIR_IN, gpio.EDGE_RISING);
+    gpio.setup(levelDownPin, gpio.DIR_IN, gpio.EDGE_RISING);
+    gpio.setup(resistUpPin, gpio.DIR_OUT);
+    gpio.setup(resistDownPin, gpio.DIR_OUT);
   
-  gpio.setup(SensorPin, gpio.DIR_IN, gpio.EDGE_RISING);
-  gpio.setup(levelUpPin, gpio.DIR_IN, gpio.EDGE_RISING);
-  gpio.setup(levelDownPin, gpio.DIR_IN, gpio.EDGE_RISING);
-  
-  this.getSpeed = function()
-  {
-    return rpm;
-  };
-  this.getLevel = function()
-  {
-    return level;
-  };
-  this.getPower = function()
-  {
-    var lowerVal = 0;
-    var upperVal = 0;
-    var idxLower = Math.floor(rpm / 10);
-    var idxUpper = Math.ceil(rpm / 10);
-    //if (idxLower > 1)
-    if (idxLower > 1 && idxLower <= 12)
-    {
-      lowerVal = power[level - 1][idxLower - 2];
+    this.getSpeed = function() {
+      return rpm;
+    };
+
+    this.getLevel = function() {
+      return level;
+    };
+
+    this.setLevel = function(theLevel) {
+      level = theLevel;
+    };
+
+    this.decLevel = function() {
+      level--;
+    };
+
+    this.incLevel = function() {
+      level++;
+    };
+
+    this.getPower = function() {
+        var lowerVal = 0;
+        var upperVal = 0;
+        var idxLower = Math.floor(rpm / 10);
+        var idxUpper = Math.ceil(rpm / 10);
+        var result;
+ 
+        if (idxLower > 1 && idxLower <= 12) {
+            lowerVal = power[level - 1][idxLower - 2];
+        } else {
+            idxLower = 0;
+        }
+
+        if (idxUpper > 1 && idxUpper <= 12) {
+            upperVal = power[level - 1][idxUpper - 2];
+
+        } else if (idxUpper > 20) {
+            // Happens when RPM > 200.
+            //console.log("RPM(", rpm, ") out of range");
+            rpm = oldRPM; //rpm /= 4.5; // approximate down to 53RPM, not zero.
+        } else if (idxUpper <= 1) {
+             idxUpper = 0;
+        } else {
+            oldRPM = rpm; // The last known good rpm value.
+        }
+
+        if (idxUpper == 0 && idxLower == 0) {
+          return 0;
+          // console.log("----------- idxUpper == 0 && idxLower == 0 ------------");
+        }
+                
+        result = (upperVal - lowerVal) / (idxUpper * 10 - idxLower * 10) * (rpm - idxLower * 10) + lowerVal;
+
+        // Debugging
+        /*if (idxUpper == idxLower) {
+          console.log(">>>>>>>>>>> idxUpper == idxLower <<<<<<<<<<");
+        }
+        if (upperVal == lowerVal) {
+          console.log("[[[[[[[[[[[ upperVal == lowerVal ]]]]]]]]]]");
+        } */
+        /* if (isNaN(result)) {
+          console.log("============ RESULT IS NaN =============");
+        } */
+
+        // Fix for power drop out.
+        if(result==0 || isNaN(result)) {
+          /* console.log("===================================");
+          console.log("upperVal = " + upperVal);
+          console.log("lowerVal = " + lowerVal);
+          console.log("idxUpper = " + idxUpper);
+          console.log("idxLower = " + idxLower);
+          console.log("rpm = " + rpm);
+          console.log("==================================="); */
+        } else {
+            /* Store the last known good value.
+               This solves issue of the appearance of a power dropout.
+               It is not an ANT+ issue.
+               Necessary for the occasional result equaling zero or NaN 
+               whenever upperVal==lowerVal or idxUpper==idxLower.
+            */
+            oldPower = result; 
+        }
+        
+        return result || oldPower; // Only return a positive number.
+      };
+      
+    this.getMaxLevel = function() {
+        return maxLevel;
     }
-    else
-    {
-      idxLower = 0;
+    
+    this.setAutoMode = function(value) {
+        autoMode = value;
     }
-    if (idxUpper > 1 && idxUpper <= 12) //12
-    {
-      upperVal = power[level - 1][idxUpper - 2];
-    }
-    else if (idxUpper > 12) //12
-    {
-      console.log("RPM(", rpm, ") out of range");
-      return power[level - 1][10];
-    }
-    else if (idxUpper <= 1)
-    {
-      idxUpper = 0;
-    }
-    if (idxUpper == 0 && idxLower == 0)
-    {
-      return 0;
-    }
-    return (upperVal - lowerVal) / (idxUpper * 10 - idxLower * 10) * (rpm - idxLower * 10) + lowerVal;
-  };
 };
+
+// If using a reed switch, 0 is on, and 1 is off, and relay = new onoff(theGPIOpin, 'in');
+// If using a hall sensor, 1 is on, and 0 is off, and relay = new onoff(theGPIOpin, 'out');
+SpeedMeter.prototype.toggleRelay = function(theGPIOpin, delay) {
+    relay = new onoff(theGPIOpin, 'out'); //use GPIO pin #, and specify that it is output.
+    relay.writeSync(1); // activate relay
+    sleep(delay);
+    relay.writeSync(0); // deactivate relay
+    sleep(delay);
+}
+
 module.exports.SpeedMeter = SpeedMeter;
---
+++ Zwifter.js
@@ -0,0 +1,111 @@
+var username = "";
+var password = "";
+var riderID = "";
+var startingLevel = 8; // The level at zero degrees grade.  If too easy, increase value.
+var maxLevel; // = 18; // the quantity of rows in the array in Speedmeter.js  The highest resistance level of your machine.
+
+// -------------- Do not edit below this line --------------
+
+var ZwiftAccount = require("zwift-mobile-api");
+var account = new ZwiftAccount(username, password);
+var world = account.getWorld(1);
+var timeOut = 2; // Do not change this value, else grade will be terribly inaccurate!
+var tempGrade;
+var tempLevel;
+var xOld;
+var yOld;
+var altitudeOld;
+var level; // The requested level to move to.
+var slope;
+var easyMode = false;
+
+//Constructor
+var Zwifter = function() {
+    // empty.
+};
+
+Zwifter.prototype.start = function() {
+    refreshData();
+}
+
+// run this function every x second(s)
+function refreshData() {
+    var floatSlope;
+    var slopeAlt;
+    //console.log("REFRESHING DATA");
+    world.riderStatus(riderID).then(status => {
+        var xNow = status.riderStatus.x;
+        var yNow = status.riderStatus.y;
+        var altitudeNow = status.riderStatus.altitude;
+        xOld = xOld || xNow;
+        yOld = yOld || yNow;
+        altitudeOld = altitudeOld || altitudeNow;
+        var rise = altitudeNow - altitudeOld;
+        var run = Math.sqrt(Math.pow((xNow - xOld), 2) + Math.pow((yNow - yOld), 2));
+        if (run != 0) {
+            floatSlope = (rise / run) * 50; // Percentage, but divided by 2, hence 50=100/2.  By design on Zwift.
+            slopeAlt = (rise / run) * 100; // Percentage, but divided by 2, hence 50=100/2.  By design on Zwift.
+            slopeAlt = Math.round(slopeAlt);
+            slope = Math.round(floatSlope);
+        } else {
+            floatSlope = 0;
+            slope = 0;
+        }
+        
+        // Debugging ///////////////////////////////
+        //console.log("grade: " + slope + "%,  Alt: " + slopeAlt + "%");
+        //console.log(floatSlope + "%");
+        //console.log(" ");
+        ////////////////////////////////////////////
+        
+        tempLevel = startingLevel + slope; // tempLevel is relative to the startingLevel at slop value zero.
+        if (slope == 0) {
+            level = startingLevel;
+        } else if (slope > 0) {
+            if(tempLevel > maxLevel) {
+                level = maxLevel;
+            } else {
+                level = tempLevel;
+            }
+        } else { slope < 0
+            if (tempLevel < 1) {
+                level = 1;
+            } else {
+                level = tempLevel;
+            }
+        }
+        // Lastly, the current values become the old values for the next loop itteration.
+        xOld = xNow;
+        yOld = yNow;
+        altitudeOld = altitudeNow;
+    });
+    setTimeout(refreshData, timeOut*1000); //infinite loop.
+}
+
+Zwifter.prototype.getLevelRequested = function() {
+    //level = level || startingLevel;
+    return level;
+}
+
+Zwifter.prototype.getGrade = function() {
+    //slope = slope || 0;
+    return slope;
+}
+
+Zwifter.prototype.getLevelAtZeroPercentGrade = function() {
+    return startingLevel;
+}
+
+Zwifter.prototype.setLevelAtZeroPercentGrade = function(value) {
+    startingLevel = value;
+}
+
+Zwifter.prototype.setEasyMode = function(value) {
+    easyMode = value;
+}
+
+Zwifter.prototype.setMaxLevel = function(value) {
+    maxLevel = value;
+}
+
+module.exports.Zwifter = Zwifter;
